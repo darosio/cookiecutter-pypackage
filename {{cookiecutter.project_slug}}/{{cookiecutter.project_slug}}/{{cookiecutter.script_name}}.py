d10e8fbabb50641c9033a73da7fc8b3308e2e5e5
daniele arosio <daniele.arosio@cnr.it>
2016-04-14 14:56:42 +0200

    import line into script file

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.script_name}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.script_name}}.py
@@ -1,9 +1,8 @@
 '''{{ cookiecutter.script_name }}
 
 Usage:
-  dark ship new <name>...
+  dark [options] <name>
   dark ship <name> move <x> <y> [--speed=<kn>]
-  dark ship shoot <x> <y>
   dark mine (set|remove) <x> <y> [--moored|--drifting]
   dark -h | --help
   dark --version
@@ -16,8 +15,10 @@ Options:
   --drifting    Drifting mine.
 '''
 
-#from __future__ import unicode_literals, print_function
+# from __future__ import unicode_literals, print_function
 from docopt import docopt
+from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
+
 
 __version__ = "{{ cookiecutter.version }}"
 __author__ = "{{ cookiecutter.full_name }}"
